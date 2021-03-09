b6e1db3c6d99ddd3d160e281bd2c1d46bce85049
thiagopetherson <thiagopetherson@outlook.com>
2021-03-09 01:25:17 -0300

    acerto

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,22 +1,9 @@
 import Vue from 'vue' //Importando o Vue
 import axios from 'axios' //Importando o Axios
 
-//Definindo a URL base (para acessarmos o backend) que usaremos o AXIOS 
-//axios.defaults.baseURL = 'https://curso-vue-27c39-default-rtdb.firebaseio.com/'
-
-//Criando os headers (se fossemos usar o Axios de forma global)
-//axios.defaults.headers.common['Authorization']  = 'abc123'
-//Acima, quer dizer que em qualquer tipo de requisição mandaremos o 'Authorization'
-//Aquele 'abc123' pode ser nosso token que recebemos após um login, por exemplo
-//axios.defaults.headers.get['Accepts'] = 'application/json'
-//Acima estamos dizendo que todas as nossas requisições GET terão o 'application/json'
-
 Vue.use({
     install(Vue) {
-        //Essa propriedade ($http) que estará disponível... 
-        //...globalmente para utilizarmos o Axios com essa url por padrao
-        //Vue.prototype.$http = axios 
-
+   
         //Criando uma instância do Axios
         Vue.prototype.$http = axios.create({
             baseURL: 'https://curso-vue-27c39-default-rtdb.firebaseio.com/',
@@ -28,13 +15,14 @@ Vue.use({
         //Interceptando as requisições
         //A função config (podemos usar o nome que quisermos pra essa função) tem as informações da nossa requisição
         Vue.prototype.$http.interceptors.request.use(config => { 
-            console.log(config.method) 
-            return config         
+            
+            return config       
+              
         }, error => Promise.reject(error))
 
         //Interceptando as respostas        
-        Vue.prototype.$http.interceptors.response.use(resp => {            
-     
+        Vue.prototype.$http.interceptors.response.use(resp => {
+
            return resp
 
         }, error => Promise.reject(error))
